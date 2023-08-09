4e8913bd7376f96b0949df98a9825c0e59ef5aa4
Ahmed Ayman <95429474+a7mdayman2002@users.noreply.github.com>
2023-08-09 16:34:18 +0300

    Delete Simple-Online-Bakery-Store-main directory

--- Simple-Online-Bakery-Store-main/db/mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose =require('mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017/students",
-{useNewUrlParser:true,useUnifiedTopology:true
-
-}).then(()=>{
-    console.log("connection success");
-}).catch((error)=>{
-    console.log("something wrong",error);
-})
